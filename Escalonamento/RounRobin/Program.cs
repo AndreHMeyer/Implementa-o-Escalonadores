1356954bd554da525488a0c973b81b18a61857e5
André Meyer <andrehmeyer11@gmail.com>
2024-05-23 00:19:41 -0300

    Escalonadores

---
+++ Escalonamento/RounRobin/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace RoundRobin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\AndréMeyer\Downloads\RrSchedule.txt";
+
+            Driver.Start(path);
+        }
+    }
+}
